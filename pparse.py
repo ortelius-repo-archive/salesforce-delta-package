9e59d9ab2dbf40e89df3d1668b8032904f6316de
Steve Taylor <steve@deployhub.com>
2021-09-22 18:57:28 -0600

    ren file

--- pparse.py
+++ pparse.py
@@ -1,32 +1,45 @@
-# Program to convert an xml
-# file to json file
- 
-# import json module and xmltodict
-# module provided by python
+import copy
 import json
-import xmltodict
+import subprocess
 from pprint import pprint
- 
-# open the input xml file and read
-# data in form of python dictionary
-# using xmltodict module
-with open('/Users/steve/git/pparse/package.xml') as xml_file:
-     
-    data_dict = xmltodict.parse(xml_file.read())
-    xml_file.close()
-     
-    package = data_dict.get('Package')
-    types = package.get('types')
 
-    for atype in (types):
-        name = atype.get('name')
-        members = atype.get('members') 
+import click
+import xmltodict
+
+
+@click.command()
+@click.option('--fromcommit', help='From commit or branch', required=True)
+@click.option('--tocommit', help='To commit or branch', required=True)
+@click.option('--packagexml', help='package.xml file name and path', required=True)
+
+def main(fromcommit, tocommit, packagexml):
+
+    filelist = subprocess.run(['git', 'diff', '--name-only', fromcommit, tocommit], check=False, stdout=subprocess.PIPE).stdout.decode('utf-8') 
+
+    # open the input xml file and read
+    # data in form of python dictionary
+    # using xmltodict module
+    with open(packagexml) as xml_file:
         
-        for mem in members:
-            print(name + "/" + mem + ".class")
-            text_file = open(name + "/" + mem + ".class", "w")
-            text_file.write('Welcome to pythonexamples.org')
-            text_file.close()
+        data_dict = xmltodict.parse(xml_file.read())
+        clean_dict = copy.deepcopy(data_dict)
 
-    # generate the object using json.dumps()
-    # corresponding to json data
+        xml_file.close()
+        
+        package = data_dict.get('Package')
+        types = package.get('types')
+
+        pos = 0
+        for atype in (types):
+            name = atype.get('name')
+            members = atype.get('members') 
+            
+            for mem in members:
+                lookup = name + "/" + mem + ".class"
+                if (lookup in filelist):
+                    print(str(pos) + " " + lookup)
+                    pprint(data_dict['Package']['types'])
+            pos = pos + 1
+
+if __name__ == '__main__':
+    main()
